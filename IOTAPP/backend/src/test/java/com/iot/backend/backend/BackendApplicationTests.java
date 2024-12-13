36b80aaa9468d3d85f6f6b1b7cbbbfc18ba61bd3
Jimmy <a0970124936@gmail.com>
2024-12-13 16:02:10 +0800

    first commit

--- IOTAPP/backend/src/test/java/com/iot/backend/backend/BackendApplicationTests.java
+++ IOTAPP/backend/src/test/java/com/iot/backend/backend/BackendApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.iotapp;
+package com.iot.backend.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IotappApplicationTests {
+class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
