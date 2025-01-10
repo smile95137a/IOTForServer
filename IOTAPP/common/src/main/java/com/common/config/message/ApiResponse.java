d0a9964eedd5baa765861d148ca2c9b30f0d4aa6
unhappyjavamansfriend <dunhull29@gmail.com>
2025-01-10 16:26:30 +0800

    add common ApiResponse

---
+++ IOTAPP/common/src/main/java/com/common/config/message/ApiResponse.java
@@ -0,0 +1,18 @@
+package com.common.config.message;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ApiResponse<T> implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+    private int code;
+    private String message;
+    private boolean success;
+    private T data;
+}
