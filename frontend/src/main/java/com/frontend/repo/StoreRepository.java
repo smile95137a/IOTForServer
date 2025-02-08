9cbe9573854164d08de20c349552eb664dbf8232
jimmy tsai <a0970124936@gmail.com>
2025-02-08 14:45:55 +0800

    no message

--- frontend/src/main/java/com/frontend/repo/StoreRepository.java
+++ frontend/src/main/java/com/frontend/repo/StoreRepository.java
@@ -15,13 +15,20 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
 
     Optional<Store> findByUid(String uid);
 
-    @Query(value = "SELECT new com.frontend.res.store.StoreRes(s.id, s.uid, s.address, s.name,s.lat , s.lon, " +
-            "COUNT(CASE WHEN pt.status = :status THEN 1 ELSE NULL END), " +
-            "COUNT(CASE WHEN pt.status != :status THEN 1 ELSE NULL END)) " +
+    @Query(value = "SELECT new com.frontend.res.store.StoreRes(s.id, s.uid, s.address, s.name, " +
+            "COUNT(CASE WHEN pt.status = :status THEN 1 ELSE NULL END), " + // availablesCount
+            "COUNT(CASE WHEN pt.status != :status THEN 1 ELSE NULL END), " + // inusesCount
+            "s.lat, s.lon, " +
+            "s.regularRate, s.discountRate, " +
+            "s.regularDateRange, s.discountDateRange, " +
+            "s.regularTimeRange, s.discountTimeRange, " +
+            "s.deposit) " +
             "FROM Store s " +
             "JOIN s.poolTables pt " +
             "WHERE s.uid = :uid " +
-            "GROUP BY s.id, s.uid, s.address, s.name")
+            "GROUP BY s.id, s.uid, s.address, s.name, s.lat, s.lon, " +
+            "s.regularRate, s.discountRate, s.regularDateRange, s.discountDateRange, " +
+            "s.regularTimeRange, s.discountTimeRange, s.deposit")
     Optional<List<StoreRes>> countAvailableAndInUseByUid(@Param("uid") String uid, @Param("status") String status);
 
 
