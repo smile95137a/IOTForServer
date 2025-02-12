8e3394af67f9d92490d5444800b0fa27a9263ef5
jimmy tsai <a0970124936@gmail.com>
2025-02-12 18:43:54 +0800

    no message

---
+++ frontend/src/main/java/com/frontend/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.frontend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${pictureFile.path}")
+    private String filePath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")  // 匹配所有路徑
+                .addResourceLocations("file:" + filePath + "/")  // 實際目錄
+                .setCacheControl(CacheControl.maxAge(1, TimeUnit.HOURS));
+    }
+}
