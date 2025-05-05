9e6c7bd95f7c77de5d06737c0c6af0574afc1472
jimmy tsai <a0970124936@gmail.com>
2025-05-05 23:23:03 +0800

    no message

--- frontend/src/main/java/com/frontend/res/store/SpecialTimeSlotRes.java
+++ frontend/src/main/java/com/frontend/res/store/SpecialTimeSlotRes.java
@@ -14,9 +14,9 @@ import java.time.LocalTime;
 public class SpecialTimeSlotRes {
 
     private Long id;  // 特殊時段的 ID
-    @JsonFormat(pattern = "HH:mm:ss")
+    @JsonFormat(pattern = "HH:mm")
     private LocalTime startTime;  // 開始時間
-    @JsonFormat(pattern = "HH:mm:ss")
+    @JsonFormat(pattern = "HH:mm")
     private LocalTime endTime;  // 結束時間
     private Boolean isDiscount;  // 是否為折扣時段
     private Integer price;  // 該時段的價格
