647559e615e9df73cdc2fe6ccaea55c81169ff4e
jimmy tsai <a0970124936@gmail.com>
2025-05-05 23:26:28 +0800

    no message

--- frontend/src/main/java/com/frontend/res/store/SpecialDateRes.java
+++ frontend/src/main/java/com/frontend/res/store/SpecialDateRes.java
@@ -16,9 +16,9 @@ public class SpecialDateRes {
 
     private Long id;  // 特殊日期 ID
     private String date;  // 特殊日期（可以使用 String 格式化 LocalDate）
-    @JsonFormat(pattern = "HH:mm:ss")
+    @JsonFormat(pattern = "HH:mm")
     private LocalTime openTime;  // 開放時間
-    @JsonFormat(pattern = "HH:mm:ss")
+    @JsonFormat(pattern = "HH:mm")
     private LocalTime closeTime;  // 關閉時間
     private Integer regularRate;  // 正常時段的費用（如果有）
     private List<SpecialTimeSlotRes> timeSlots;  // 特殊時段列表
